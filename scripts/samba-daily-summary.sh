a4abc218827af133d26f82d42ff5244e1ed0608c
SecuriLee <117658191+SecuriLee@users.noreply.github.com>
2023-12-15 13:09:22 +0100

    Update samba-daily-summary.sh

--- scripts/samba-daily-summary.sh
+++ scripts/samba-daily-summary.sh
@@ -5,37 +5,63 @@
 # Based on: https://github.com/thinkst/opencanary
 # Read more: www.toce.ch and www.willigetpwned.com
 
-# Set the path to your log file
-LOG_FILE="/var/log/samba-audit.log"
+log_file="/var/log/samba-audit.log"
+summary_log="$HOME/logs/samba-daily-summary.log"
+webhook_url="YOUR_WEBHOOK_URL"
 
-# Set the webhook URL
-WEBHOOK_URL="https://your.webhook.url"
+# Get yesterday's date in the format "Dec 14"
+TODAY_MONTH_DAY=$(date -d "yesterday" +"%b %d")
 
-# Get today's month and day
-TODAY_MONTH_DAY=$(date +"%b %d")
+# Function to extract information from a log line
+extract_info() {
+    local line="$1"
+    local username ip_address computer_name
 
-# Count the number of lines for today
-LOG_COUNT=$(grep "$TODAY_MONTH_DAY" "$LOG_FILE" | grep "smbd_audit:" | wc -l)
+    # Extract information using awk
+    username=$(echo "$line" | awk -F' ' '{split($0,a,"smbd_audit: "); split(a[2],b,"|"); print b[1]}')
+    ip_address=$(echo "$line" | awk -F'|' '{print $2}')
+    computer_name=$(echo "$line" | awk -F'|' '{print $4}')
 
-# Check if there are no logs for TODAY_MONTH_DAY and modify the message accordingly
-if [ "$LOG_COUNT" -eq 0 ]; then
-    MESSAGE="[OC/Loc] Samba Access Summary\n$TODAY_MONTH_DAY: Logs appear empty"
-else
-    # Process each log entry and create the summary
-    SUMMARY=""
-    while IFS= read -r line; do
-        username=$(echo "$line" | awk -F'smbd_audit: |\\|' '{print $2}')
-        # Replace empty usernames with "<blank>"
-        [ -z "$username" ] && username="<blank>"
-        ip_address=$(echo "$line" | awk -F'|' '{print $2}')
-        computer_name=$(echo "$line" | awk -F'|' '{print $4}')
-        SUMMARY="${SUMMARY}${username};${ip_address};${computer_name}\n"
-    done <<< "$LOG_ENTRIES"
+    # Log debug information to the summary log
+    echo "Debug: Search string: $line" >> "$summary_log"
+    echo "Debug: Line starting with search string: $line" >> "$summary_log"
 
-    # Output the final summary (unique list without repetition)
-    FINAL_SUMMARY=$(echo -e "$SUMMARY" | sort -u)
-    MESSAGE="[OC/Loc] Samba Access Summary\n$FINAL_SUMMARY"
-fi
+    # Update dictionaries
+    [[ -n $username ]] && ((usernames["$username"]++))
+    [[ -n $ip_address ]] && ((ip_addresses["$ip_address"]++))
+    [[ -n $computer_name ]] && ((computers["$computer_name"]++))
+}
 
-# Send the webhook message
-curl -X POST -H "Content-Type: application/json" -d "{\"text\":\"$MESSAGE\"}" "$WEBHOOK_URL"
+# Initialize arrays to store unique values
+declare -A usernames
+declare -A ip_addresses
+declare -A computers
+
+# Read the log file line by line
+while IFS= read -r line; do
+    # Check if the line matches yesterday's date format
+    if [[ $line =~ ^$TODAY_MONTH_DAY ]]; then
+        extract_info "$line"
+    fi
+done < "$log_file"
+
+# Construct the payload for the webhook
+payload="{\"text\":\"[OC/Loc] Samba Access Summary for $TODAY_MONTH_DAY\n\nList of Usernames:\n"
+for username in "${!usernames[@]}"; do
+    payload+=" $username: ${usernames[$username]} occurrences\n"
+done
+
+payload+="\nList of IP Addresses:\n"
+for ip_address in "${!ip_addresses[@]}"; do
+    payload+=" $ip_address: ${ip_addresses[$ip_address]} occurrences\n"
+done
+
+payload+="\nList of Computers:\n"
+for computer_name in "${!computers[@]}"; do
+    payload+=" $computer_name: ${computers[$computer_name]} occurrences\n"
+done
+
+payload+="\"}"
+
+# Send the payload to the webhook
+curl -X POST -H "Content-Type: application/json" -d "$payload" "$webhook_url"
